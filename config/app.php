0137d2d3ecfa4e4eb375d7678ea981588665636f
akimoto_aki <alhs1995@gmail.com>
2015-12-19 14:43:20 +0800

    fix bug

--- config/app.php
+++ config/app.php
@@ -205,7 +205,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Form'      => Collective\Html\FormFacade::class,
-        'HTML'      => Collective\Html\HtmlFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
         'Carbon'    => Carbon\Carbon::class,
         'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
         'Minify'    => Devfactory\Minify\Facades\MinifyFacade::class,
